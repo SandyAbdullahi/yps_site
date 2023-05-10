7d8c4aabbe50fb3e447ec71feac089061d803bd0
Abdullahinur Ahmed Abdullahi <ab2d222@gmail.com>
2023-05-10 18:57:00 +0300

    add scrolling text section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -29,14 +29,14 @@ const HeroSection = () => {
         className="absolute bottom-0 right-0 z-10 text-white text-right pr-8 pb-8"
      
       >
-        <motion.h1 className="text-6xl sm:text-5xl lg:text-6xl xl:text-8xl font-bold mb-6 text-primary"
+        <motion.h1 className="text-6xl sm:text-5xl lg:text-6xl xl:text-8xl 2xl:text-9xl font-bold mb-6 text-primary"
          initial = {{ y: 100, opacity: 0 }}
          animate = {{ y: 0, opacity: 1 }}
          transition={{duration: 1, delay: 0}}
         >
-          Unleash Your Film Dreams
+          Unleash Your <br /> Film Dreams
         </motion.h1>
-        <motion.p className="text-md sm:text-xl lg:text-2xl"
+        <motion.p className="text-md sm:text-xl lg:text-2xl  xl:text-4xl 2xl:text-5xl"
             initial = {{ y: 100, opacity: 0 }}
             animate = {{ y: 0, opacity: 1 }}
             transition={{duration: 1, delay: 0.5}}
