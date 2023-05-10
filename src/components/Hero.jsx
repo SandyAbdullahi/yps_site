2c0dd9cc797ff6ba03917b597bdd03f2fdde1a72
Abdullahinur Ahmed Abdullahi <ab2d222@gmail.com>
2023-05-10 19:16:20 +0300

    add scrolling text section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,7 @@ const HeroSection = () => {
   };
 
   return (
-    <section className="h-screen flex mb-0">
+    <section className="h-screen flex -mb-20">
       <div className="absolute inset-0 z-0 bottom-0">
         <video
           className="w-full h-full object-cover"
