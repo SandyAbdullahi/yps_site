4ad5aae686cbd445bfa640a6e62fac91210ec2d3
Abdullahinur Ahmed Abdullahi <ab2d222@gmail.com>
2023-05-14 21:32:16 +0300

    add  some aniumations

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ import { motion } from 'framer-motion';
 const HeroSection = () => {
 
 return (
-<section className="h-[60vh]">
+<section className="fiexed top-0 -z-50 h-[60vh]">
 
 
 
@@ -13,12 +13,12 @@ return (
     
   
     
-    <img className="w-full h-full object-cover"   src="./YPS_BACKGROUND.gif" alt="" />
+    <img className="absolute top-0 -z-50 h-full  lg:h-[70vh] w-full object-cover"   src="./YPS_BACKGROUND.gif" alt="" />
    
   
-    <div className="bg-gradient-to-b from-secondary to-transparent opacity-50 absolute inset-0 h-[60vh]"></div>
+    <div className="bg-gradient-to-b from-secondary to-transparent opacity-50 absolute inset-0 h-[80vh]"></div>
     <div className="videoBG">
-      <div className="absolute inset-0 h-[60vh]">
+      <div className="absolute inset-0 h-[70vh] lg:h-[65vh]">
         <div className="bottom-3 absolute right-3 text-right pl-5">
           <h1 className="text-primary font-bold text-7xl">There Will Be Some</h1>
           <p className="text-white">Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
